ea66ecc753e8f9838968629efd0cdc75dc8d4428
Lucas <lucas.copnell@my.jcu.edu.au>
2019-08-21 11:47:22 +1000

    Remove global variables

--- prac_03/temperatures.py
+++ prac_03/temperatures.py
@@ -6,32 +6,34 @@ Pseudocode for temperature conversion
 MENU = """C - Convert Celsius to Fahrenheit
 F - Convert Fahrenheit to Celsius
 Q - Quit"""
-print(MENU)
-choice = input(">>> ").upper()
 
 
-def calculate_fahrenheit():
-    global fahrenheit
+def main():
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "C":
+            celsius = float(input("Celsius: "))
+            calculate_fahrenheit(celsius)
+        elif choice == "F":
+            fahrenheit = float(input("Fahrenheit: "))
+            celsius_calculator(fahrenheit)
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+
+def calculate_fahrenheit(celsius):
     fahrenheit = celsius * 9.0 / 5 + 32
     return fahrenheit
 
 
-def celsius_calcualator():
-    global celsius
+def celsius_calculator(fahrenheit):
     celsius = 5 / 9 * (fahrenheit - 32)
     return celsius
 
 
-while choice != "Q":
-    if choice == "C":
-        celsius = float(input("Celsius: "))
-        calculate_fahrenheit()
-    elif choice == "F":
-        fahrenheit = float(input("Fahrenheit: "))
-        celsius_calcualator()
-    else:
-        print("Invalid option")
-    print(MENU)
-    choice = input(">>> ").upper()
-print("Thank you.")
+
 
