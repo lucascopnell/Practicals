08610a0f6ecdde5bcf1ece7d4fef0f0aa66cf529
Lucas <lucas.copnell@my.jcu.edu.au>
2019-08-21 11:31:25 +1000

    Add get_password function

---
+++ prac_03/password_checker.py
@@ -0,0 +1,20 @@
+MIN_LENGTH = 4
+
+
+def main():
+    password = get_password()
+# asterisks_password(password)
+
+
+def get_password():
+    password = input("Please enter your password: ")
+    while len(password) < MIN_LENGTH:
+        print("Invalid Password.")
+        password = input("Please enter your password: ")
+    return password
+
+
+main()
+
+
+
