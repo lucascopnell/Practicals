3434fb37c3a078705a4e5da1a64812f7fd7ee23e
Lucas <lucas.copnell@my.jcu.edu.au>
2019-09-05 12:09:35 +1000

    Add for loop for state names

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -10,6 +10,7 @@ STATE_NAMES = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Te
 
 for state in STATE_NAMES:
     print("{:3} is {}".format(state, STATE_NAMES[state]))
+    
 
 state = input("Enter short state: ").upper()
 while state != "":
