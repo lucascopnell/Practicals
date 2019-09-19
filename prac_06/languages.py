d10cbbaeaaf3a296486a5357c49e792ba3e7667f
Lucas <lucas.copnell@my.jcu.edu.au>
2019-09-19 10:20:58 +1000

    Add check for dyamic language in language file

---
+++ prac_06/languages.py
@@ -0,0 +1,16 @@
+
+from prac_06.programming_language import ProgrammingLanguage
+
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+languages = [ruby, python, visual_basic]
+print([str(language) for language in languages])
+
+print("\n The dynamic typed languages are: ")
+dynamic_languages = [language.name for language in languages if language.is_dynamic()]
+for i in dynamic_languages:
+    print(i)
+    
+
